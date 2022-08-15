d4a85186250a86cf6b2fc6e8564e0d439ec84823
suanjin1 <110980816+suanjin1@users.noreply.github.com>
2022-08-15 21:17:54 +0800

    修改版本号更新地址

--- app/src/main/java/com/github/tvbox/osc/util/AppUpdate.java
+++ app/src/main/java/com/github/tvbox/osc/util/AppUpdate.java
@@ -16,8 +16,8 @@ import okhttp3.Response;
 public class AppUpdate {
 
     private static final String[] updateUrls = new String[] {
-            "https://raw.githubusercontent.com/kensonmiao/CatVodTVOSC_Release/main/",
-            "https://codeberg.org/kensonlogin55/CatVodTVOSC_Release/raw/branch/main/"
+            "https://raw.githubusercontent.com/suanjin1/CatVodTVOSC_Release/main/",
+            "https://codeberg.org/suanjin/CatVodTVOSC_Release/raw/branch/main/"
     };
 
     public static String getCurrentVersionNo() {
