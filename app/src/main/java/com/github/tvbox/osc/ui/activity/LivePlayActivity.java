e0b0d8d5a036bc43a34d2b69198f4f703e1a9ef9
jixiaoruo1 <112071816+jixiaoruo1@users.noreply.github.com>
2022-09-13 22:36:55 +0800

    修改直播UI

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -657,7 +657,7 @@ public class LivePlayActivity extends BaseActivity {
             ((TextView) findViewById(R.id.tv_channel_bottom_number)).setText("" + channel_Name.getChannelNum());
             ((TextView) findViewById(R.id.tv_current_program_time)).setText("咦，暂无信息");
             ((TextView) findViewById(R.id.tv_current_program_name)).setText("");
-            ((TextView) findViewById(R.id.tv_next_program_time)).setText("开源测试软件,请勿商用以及播放违法内容!!!!");
+            ((TextView) findViewById(R.id.tv_next_program_time)).setText("暂无节目信息");
             ((TextView) findViewById(R.id.tv_next_program_name)).setText("");
             if (hsEpg.containsKey(channel_Name.getChannelName())) {
                 ArrayList arrayList = (ArrayList) hsEpg.get(channel_Name.getChannelName());
